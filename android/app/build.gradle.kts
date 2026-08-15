d14623774a43c80ef0353a0a04c3e684ee169aec
Kingdon B <kingdon@urmanac.com>
2026-08-15 10:55:09 -0400

    chore(release): bump Moon Oracle to version 0.0.6 (versionCode 6)
    - Document 0.0.6 release notes in CHANGELOG.md
    - Bump versionCode to 6 and versionName to 0.0.6 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.urmanac.moonphase"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
     }
 
     buildTypes {
